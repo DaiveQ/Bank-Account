df48004c2d4334881241d095607ac58b1a7a4c17
Lost <blueskyztonight@gmail.com>
2018-10-25 05:01:10 +0000

    Added Documentation
    Slight changes for readability

--- src/bank/BankAccount.java
+++ src/bank/BankAccount.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 public class BankAccount {
 
+
+    // basic variables
 	private String firstName;
 	private String lastName;
 	private String password;
@@ -12,7 +14,12 @@ public class BankAccount {
 	private String username;
 	private int accountNum;
 	private double balance;
+	
+	// a (probably a bad) way to align new line text with the 
+	// time stamp when viewing message
 	private final String DIVIDER = "\n\t     ";
+	
+	// date format for use with messages for timestamps
 	SimpleDateFormat datef = new SimpleDateFormat("dd/MM/YYYY HH:mm");
 	
 	BankAccount(String uName, double amt, String fName, String lName, int acctNum) {
@@ -22,7 +29,7 @@ public class BankAccount {
 		lastName = lName;
 		password = setPassword();
 	}
-
+    
 	BankAccount(String uName, String fname, String lName, int accNum) {
 		username = uName;
 		accountNum = accNum;
@@ -31,8 +38,8 @@ public class BankAccount {
 		balance = 0;
 
 	}
-
-	private String getTimeStamp() {
+    
+	private String getTime() {
 		// change to server side time once server is made
 		
 		Date date = new Date();
@@ -49,10 +56,12 @@ public class BankAccount {
 
 	private String setPassword() {
 		String pswd = "";
-
+        
+        // sets beginning to the first 3 characters of firstName
 		for (int i = 0; i < 3; i++)
 			pswd += firstName.charAt(i);
 
+        // sets end to the last 2 characters of the firstName
 		for (int i = 2; i > 0; i--)
 			pswd += lastName.charAt(lastName.length() - i);
 		return pswd;
@@ -61,13 +70,14 @@ public class BankAccount {
 	public String getMessage() {
 		return message;
 	}
-
+    
+    // deposits
 	boolean deposit(double amount) {
 		if (amount > 0) {
-			message += getTimeStamp() + "A deposit of " + amount + "$ has been made.\n";
+			message += getTime() + "A deposit of " + amount + "$ has been made.\n";
 			return true;
 		} else {
-			message += getTimeStamp() + "Depost unsucsessful - invalid amount\n";
+			message += getTime() + "Depost unsucsessful - invalid amount\n";
 			return false;
 		}
 
@@ -75,12 +85,12 @@ public class BankAccount {
 
 	boolean withdraw(double amount) {
 		if (amount >= balance) {
-			message += getTimeStamp() + "A bank overdraft fee of 25$ has been added to your account." + DIVIDER +  "We thank you for your business.\n";
+			message += getTime() + "A bank overdraft fee of 25$ has been added to your account." + DIVIDER +  "We thank you for your business.\n";
 			return false;
 		}
 
 		else {
-			message += getTimeStamp() + "Withdrawel of " + amount + "$ has beem made.\n";
+			message += getTime() + "Withdrawel of " + amount + "$ has beem made.\n";
 			return true;
 		}
 	}
@@ -88,58 +98,61 @@ public class BankAccount {
 	double getBalance() {
 		return balance;
 	}
-
+    
+    // displays account information
 	void display() {
 		// shouldn't be printed out
 		System.out.println("User account num: " + getAccountNum() + "\nUser full name: " + firstName + " " + lastName
 				+ "\nUser account balance: " + balance);
 	}
-
-	boolean transferTo(double amount, double otherAcct) {
-		if (amount >= balance) {
-			message += getTimeStamp() + "Transfer of $" + amount + " to account " + otherAcct + " has been complete\n";
-			otherAcct += amount;
-			balance -= amount;
+    
+    // transfers money from one BankAccount to another
+	boolean transferTo(double amount, BankAccount otherAcc) {
+		if (amount >= balance && balance > 0) {
+			message += getTime() + "Transfer of $" + amount + " to account " + otherAcct + " has been complete\n";
+			otherAcc.balance += amount;
+			this.balance -= amount;
 			return true;
 
 		}
-
 		else {
-			message += getTimeStamp() + "Overdraft during transfer occured. Transaction not processed. "
-					+ "Please contact us for more information A fee of $25 was applied\n";
+			message += getTime() + "Overdraft during transfer occured. Transaction not processed. "
+					+ "Please contact us for more information. A fee of $25 was applied\n";
 			return false;
 		}
 	}
 
 	boolean transferFrom(double amount, double otherAcct, String pswd) {
 		boolean check = checkPassword(pswd);
-		if (balance >= amount && check == true) {
-			message += getTimeStamp() + "Transfer of $" + amount + " from account " + accountNum + " complete.\n";
+		if (balance >= amount) {
+			message += getTime() + "Transfer of $" + amount + " from account " + accountNum + " complete.\n";
 			return true;
 		}
 
 		else if (amount > balance) {
-			message += getTimeStamp() + "Transfer was unsucsessful - an overdraft fee of $25 was applied\n";
+			message += getTime() + "Transfer was unsucsessful - an overdraft fee of $25 was applied\n";
 			return false;
 		}
 
 		else if (pswd != password) {
-			message += getTimeStamp() + "Transfer was unsucsessful - please check password and try again\n";
+			message += getTime() + "Transfer was unsucsessful - please check password and try again\n";
 			return false;
 		}
 
 		return false;
 	}
-
+    
+    // should be change password as it changes the password to whatever the user inputs
+    // as long as the uesr inputed original password is correct
 	boolean resetPassword(String currentPassword, String newPassword) {
 		if (currentPassword == password) {
 			password = newPassword;
-			message += getTimeStamp() + "Password succesfully changed\n";
+			message += getTime() + "Password succesfully changed\n";
 			return true;
 		}
 
 		else {
-			message += getTimeStamp() + "Password reset unsucsessful - please try again\n";
+			message += getTime() + "Password reset unsucsessful - please try again\n";
 			return false;
 		}
 	}
@@ -150,9 +163,10 @@ public class BankAccount {
 		return false;
 
 	}
-
+    
+    // emptiess account balance
 	void emptyAccount() {
-		message += getTimeStamp() + "So much for \"leaving something for a rainy day\"\n";
+		message += getTime() + "So much for \"leaving something for a rainy day\"\n";
 		balance = 0;
 	}
 
